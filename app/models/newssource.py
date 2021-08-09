dfe0eb7386ee557fcfae11b0eee7143d59f9fd3f
David Njungo <davidnjungo3@gmail.com>
2021-08-09 12:16:14 +0300

    add  the source class and objects

---
+++ app/models/newssource.py
@@ -0,0 +1,11 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,name,description,category,url,):
+        self.name = name
+        self.description = description
+        self.category = category
+        self.url = url
+        
