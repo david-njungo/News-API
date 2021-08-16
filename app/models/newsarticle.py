b80d3e6628165c0f2dcd6bb8b6f8bbd7d9da032b
David Njungo <davidnjungo3@gmail.com>
2021-08-16 12:58:32 +0300

    create the movie sources

--- app/models/newsarticle.py
+++ app/models/newsarticle.py
@@ -1,15 +1,15 @@
-class Article:
-    '''
-    Article class to define Article Objects
-    '''
+# class Article:
+#     '''
+#     Article class to define Article Objects
+#     '''
 
-    def __init__(self,source,title,description,url,urlToImage,publishedAt,content):
-        self.source = source
-        self.title = title
-        self.description = description
-        self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-        self.content = content
+#     def __init__(self,source,title,description,url,urlToImage,):
+#         self.source = source
+#         self.title = title
+#         self.description = description
+#         self.url = url
+#         self.urlToImage = urlToImage
+        
+      
         
         
--- app/models/newssource_test.py
+++ app/models/newssource_test.py
@@ -1,21 +1,21 @@
-import unittest
-from models import newssource
-Source = newssource.Source
+# import unittest
+# from models import newssource
+# Source = newssource.Source
 
-class Source(unittest.TestCase):
-    '''
-    Test Class to test the behaviour of the Source class
-    '''
+# class Source(unittest.TestCase):
+#     '''
+#     Test Class to test the behaviour of the Source class
+#     '''
 
-    def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
-        self.new_source = Source("bbc-news","BBC NEWS","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","https://abcnews.go.com","business")
+#     def setUp(self):
+#         '''
+#         Set up method that will run before every Test
+#         '''
+#         self.new_source = Source("bbc-news","BBC NEWS","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","https://abcnews.go.com","business")
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_source,Source))
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.new_source,Source))
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
